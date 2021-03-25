98ca82065004ab54420f2b83cc53cae589423f8d
murat <muratcelik11@gmail.com>
2021-03-25 16:32:45 +0300

    via

--- src/main/java/com/muratcelik/pro/HelloController.java
+++ src/main/java/com/muratcelik/pro/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String Hello(){
+    public String Hello() {
         return "helloweb";
     }
 }
